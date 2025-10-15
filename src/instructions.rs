c6a3b352bbd401203e6e8bbf2fcceee98e225467
Edgar <nazyrov01@gmail.com>
2025-10-15 13:25:38 +0500

    add pop and push

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,4 +1,4 @@
-use crate::cpu::{Argument, Cpu};
+use crate::cpu::{Argument, Cpu, RegisterU16Label};
 
 fn ld(cpu: &mut Cpu, args: [Argument; 2]) {
     match args[1] {
@@ -10,17 +10,15 @@ fn ld(cpu: &mut Cpu, args: [Argument; 2]) {
 }
 
 fn ld_u8(cpu: &mut Cpu, args: [Argument; 2]) {
-    dbg!(args);
     let source: u8 = match args[1] {
         Argument::RegisterU8(reg) => cpu.register.read_u8(reg),
         Argument::ValueU8 => cpu.load_next_u8(),
         Argument::Address(address) => cpu.read_address(address),
         _ => unreachable!("Error source type in ld_u8 instruction"),
     };
-    dbg!(source);
     match args[0] {
-        Argument::RegisterU8(reg) => cpu.register.write_u8(reg, source),
-        Argument::Address(address) => cpu.write_address(address, source),
+        Argument::RegisterU8(reg) => cpu.register.write_u8((reg), source),
+        Argument::Address(address) => cpu.write_address((address), source),
         _ => unreachable!("Error distination type in ld_u8 istruction"),
     }
 }
@@ -34,13 +32,13 @@ fn ld_u16(cpu: &mut Cpu, args: [Argument; 2]) {
     };
 
     match args[0] {
-        Argument::RegisterU16(reg) => cpu.memory.write_u16(cpu.register.read_u16(reg), source),
+        Argument::RegisterU16(reg) => cpu.register.write_u16(reg, source),
         Argument::Address(address) => cpu.write_address_u16(address, source),
         _ => unreachable!("Error distination type in ld_u16 istruction"),
     }
 }
 
-mod test_instrations {
+mod test_ld_instrution {
     use crate::cpu::{AddressType, Cpu, Memory, RegisterU8Label, RegisterU16Label};
 
     use super::*;
@@ -60,5 +58,60 @@ mod test_instrations {
             ],
         );
         assert_eq!(cpu.memory.read_u8(0x100), 1);
+
+        cpu.register.pc = 0x101;
+        cpu.memory.write_u16(0x101, 0x102);
+        ld(
+            &mut cpu,
+            [
+                Argument::RegisterU16(RegisterU16Label::DE),
+                Argument::ValueU16,
+            ],
+        );
+        assert_eq!(cpu.register.read_u16(RegisterU16Label::DE), 0x102)
+    }
+}
+
+fn push(cpu: &mut Cpu, args: [Argument; 1]) {
+    let mut register: RegisterU16Label;
+    match args[0] {
+        Argument::RegisterU16(reg) => register = reg,
+        _ => unreachable!("Error source type in push instruction"),
+    }
+    let source = cpu.register.read_u16(register);
+
+    cpu.register.sp -= 1;
+    cpu.memory.write_u8(cpu.register.sp, (source >> 8) as u8);
+    cpu.register.sp -= 1;
+    cpu.memory.write_u8(cpu.register.sp, (source & 0xFF) as u8);
+}
+
+fn pop(cpu: &mut Cpu, args: [Argument; 1]) {
+    let mut result: u16 = 0;
+    result += (cpu.memory.read_u8(cpu.register.sp) as u16);
+    cpu.register.sp += 1;
+    result += (cpu.memory.read_u8(cpu.register.sp) as u16) << 8;
+    cpu.register.sp += 1;
+
+    match args[0] {
+        Argument::RegisterU16(reg) => cpu.register.write_u16(reg, result),
+        _ => unreachable!("Error source type in pop instrution"),
+    }
+}
+
+mod test_push_instrution {
+    use crate::cpu::Memory;
+
+    use super::*;
+
+    #[test]
+    fn pop_push_inst() {
+        let mut cpu = Cpu::new(Memory::new());
+
+        cpu.register.write_u16(RegisterU16Label::HL, 0x102);
+        push(&mut cpu, [Argument::RegisterU16(RegisterU16Label::HL)]);
+        assert_eq!(cpu.memory.read_u16(cpu.register.sp), 0x102);
+        pop(&mut cpu, [Argument::RegisterU16(RegisterU16Label::BC)]);
+        assert_eq!(cpu.register.read_u16(RegisterU16Label::BC), 0x102)
     }
 }
